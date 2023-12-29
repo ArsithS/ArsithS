a4c7a4e3f99debde1938738ee405769ae822ad75
ArsithS <106135244+ArsithS@users.noreply.github.com>
2023-12-29 11:29:52 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,9 @@
-echo "hello world !!!"
+#!/bin/bash
+
+# Specify the number of terminals you want to open
+num_terminals=20
+
+# Loop to open terminals
+for ((i=1; i<=$num_terminals; i++)); do
+    gnome-terminal &
+done
